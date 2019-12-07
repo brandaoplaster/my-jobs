b3baf59a25e9b8c5f3c03ff1a9d66be96b1f3457
brandaoplaster <brandaoplaster@gmail.com>
2019-12-07 00:15:03 -0300

    Added validations in attributes

---
+++ app/models/education.rb
@@ -0,0 +1,8 @@
+class Education < ApplicationRecord
+  belongs_to :portfolio
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :institution, presence: true
+  validates :start_date, presence: true
+end
