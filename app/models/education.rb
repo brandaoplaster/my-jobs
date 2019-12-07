de284e4a8d6dec3da43ba664f9140c6eb75152b1
brandaoplaster <brandaoplaster@gmail.com>
2019-12-07 18:45:24 -0300

    Added custom validate DateIntervalValidator in the Education model

--- app/models/education.rb
+++ app/models/education.rb
@@ -5,4 +5,5 @@ class Education < ApplicationRecord
   validates :description, presence: true
   validates :institution, presence: true
   validates :start_date, presence: true
+  validates_with DateIntervalValidator
 end
