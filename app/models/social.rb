e95f86c44825749fe688267cc676233030dc94be
brandaoplaster <brandaoplaster@gmail.com>
2019-12-09 23:02:19 -0300

    Included validation for url and made relationship of model Social with model Portfolio

--- app/models/social.rb
+++ app/models/social.rb
@@ -2,4 +2,6 @@ class Social < ApplicationRecord
   belongs_to :portfolio
 
   enum kind: [:facebook, :twitter, :linkedin, :github, :other, :instagram]
+
+  validates :url, presence: true, http_url: true
 end
