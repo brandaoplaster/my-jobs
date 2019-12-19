7f1cde4305c39e3c9a1d33c7cc6c45642f60abc5
brandaoplaster <brandaoplaster@gmail.com>
2019-12-18 22:44:02 -0300

    Implemented Portfolios controller created index method and included a before_action that loads portfolios only in index method. Included in routes the route to portfolios

---
+++ app/controllers/portfolios_controller.rb
@@ -0,0 +1,8 @@
+class PortfoliosController < ApplicationController
+  before_action :load_portfolios, only: :index
+
+  def index
+    authorize(Portfolio)
+    @portfolios = policy_scope(Portfolio)
+  end
+end
