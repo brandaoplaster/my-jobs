1d09aeac9e24ed6a49a3cc9dfd832ec79c641954
brandaoplaster <brandaoplaster@gmail.com>
2020-01-17 00:08:40 -0300

    Updated portfolio steps, incluing new steps

--- features/step_definitions/portfolio_steps.rb
+++ features/step_definitions/portfolio_steps.rb
@@ -17,3 +17,20 @@ end
 Então "volta para a página de login" do
   expect(page).to have_current_path("/users/sign_in")
 end
+
+
+Quando "clica para criar um novo portfólio" do
+  find(:xpath, "//a[@class='new-portfolio']").click
+end
+
+Então "o usuário deve ter {int} portfólios cadastrados" do |count|
+  expect(@user.portfolios.count).to eq count
+end
+
+Então "deve ver os dados iniciais do portfólios" do
+  expect(page).to have_field("portfolio[slug]", with: @user.portfolios.last.slug)
+end
+
+Então "é direcionado para a página de edição do portfólio" do
+  expect(page).to have_current_path(edit_portfolio_path(@user.portfolios.last))
+end
