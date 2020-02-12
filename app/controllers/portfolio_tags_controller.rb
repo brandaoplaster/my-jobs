1037b270cd06b2b51a5a7a79c7da548c56c4d209
brandaoplaster <brandaoplaster@gmail.com>
2020-02-11 22:54:01 -0300

    Implemeted action create and two private methods save_portfolio_tag and build_portfolio_tag

--- app/controllers/portfolio_tags_controller.rb
+++ app/controllers/portfolio_tags_controller.rb
@@ -1,10 +1,25 @@
 class PortfolioTagsController < ApplicationController
 
   def create
-
+    build_portfolio_tag
+    authorize @portfolio_tag
+    save_portfolio_tag || render_error(@portfolio_tag.errors.full_messages)
   end
 
   def destroy
 
+  end
+
+  private
+
+  def save_portfolio_tag
+    if @portfolio_tag.save
+      head :ok
+    end
+  end
+
+  def build_portfolio_tag
+    @portfolio_tag = portfolio_tags_scope.build
+    @portfolio_tag.tag_id = params[:tags_id]
   end
 end
