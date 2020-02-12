cff2a60f63e1a02a8d19cf79e21377b1740a179b
brandaoplaster <brandaoplaster@gmail.com>
2020-02-11 23:30:41 -0300

    Fixed syntax error in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
   devise_for :users
-  resource :portfolios, except: :new do
-    resource :tags, only: [:create, destroy], param: :tag_id, controller: :portfolio_tags
+  resources :portfolios, except: :new do
+    resources :tags, only: [:create, :destroy], param: :tag_id, controller: :portfolio_tags
   end
-  resource :tags, only: :index
+  resources :tags, only: :index
 end
