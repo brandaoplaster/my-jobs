f53e1734a533f2a0769da4e0affc753227babfb5
brandaoplaster <brandaoplaster@gmail.com>
2020-02-13 23:03:17 -0300

    Created the HTML structure with the inputs

---
+++ app/javascript/packs/components/tags.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="row">
+        <div class="input-field col s12">
+            <input type="text" class="autocomplete" @keypress="addTag" id="autocomplete-tags">
+            <label for="autocomplete-tags">Procure uma Tag</label>
+        </div>
+        <div class="col s12">
+            <div class="chip" v-for="tag in portfolioTags" v-bind:key="tag">
+                {{ tag.title }}
+                <i class="close material-icons" @click="removeTag(tag)">close</i>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['portfolioId', 'portfolioTags'],
+
+    data() {
+        return {
+            currentTag: "",
+            tags: []
+        }
+        
+    }   
+}
+</script>
