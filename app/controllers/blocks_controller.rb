02bf020bdda360432e448cfd6b1223148c802589
brandaoplaster <brandaoplaster@gmail.com>
2020-05-26 20:56:26 -0300

    Action index that returns all the blocks in the portfolio that are retrieved throught the load_portfolio callback

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -1,8 +1,14 @@
 class BlocksController < ApplicationController
 	before_action :load_portfolio
 
+	def index
+		render json: { blocks: @portfolio.blocks }
+	end
+
 	private
 
 	def load_portfolio
+		@portfolio = policy_scope(Portfolio).find(params[:portfolio_id])
+		authorize @portfolio
 	end
 end
