b67bd1a42cf52d4026a49d556bb30c700ba96d56
brandaoplaster <brandaoplaster@gmail.com>
2020-05-29 22:20:41 -0300

    Implementing private mthods to render resource and error message

--- app/controllers/portfolio_resources_controller.rb
+++ app/controllers/portfolio_resources_controller.rb
@@ -3,6 +3,14 @@ class PortfolioResourcesController < ApplicationController
 
   private
     
+    def render_resource
+      render json: { resource: resource }
+    end
+
+    def render_error(message)
+      render json: { errors: message }, status: :unprocessable_entity
+    end
+
     def load_portfolio
       @portfolio = Portfolio.find(params[:portfolio_id])
     end
