bb53ef12f7bb093c537af63819777473549c5f6b
brandaoplaster <brandaoplaster@gmail.com>
2020-05-29 22:23:58 -0300

    Creating private methods to assist action create and destroy

--- app/controllers/portfolio_resources_controller.rb
+++ app/controllers/portfolio_resources_controller.rb
@@ -2,7 +2,19 @@ class PortfolioResourcesController < ApplicationController
   before_action :load_portfolio
 
   private
-    
+  
+    def save_resource
+      if @resource.save
+        render_resource(@resource)
+      end
+    end
+
+    def destroy_resource
+      if @resource.destroy
+        head :ok
+      end
+    end
+
     def render_resource
       render json: { resource: resource }
     end
