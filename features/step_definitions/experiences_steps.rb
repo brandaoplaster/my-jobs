c97ad93210ae8800a451ca642889dbe8526b9ae0
brandaoplaster <brandaoplaster@gmail.com>
2020-06-01 22:47:17 -0300

    Update stpes experiences added the removal scenario

--- features/step_definitions/experiences_steps.rb
+++ features/step_definitions/experiences_steps.rb
@@ -17,3 +17,20 @@ Quando "adiciona uma Experiência" do
   find("#add-experience").click
   wait_for_ajax
 end
+
+Quando "remove a Experiência" do
+  find(".remove-experience").click
+  wait_for_ajax
+end
+
+Então "a Experiência é adiconada ao Portfólio" do
+  @portfolio.reload
+  expect(@portfolio.experiences.count).to eq 1
+  expect(page).to have_css(".resource-experience", count: 1)  
+end
+
+Então "a Experiência é removida do Portfólio" do
+  @portfolio.reload
+  expect(@portfolio.experiences.count).to eq 0
+  expect(page).to_not have_css(".resource-experience")
+end
