fcb9101b201099465a20117a73f029eaabbe3242
brandaoplaster <brandaoplaster@gmail.com>
2020-06-02 21:40:13 -0300

    Including load portfolio resource method responsible for finding experience

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -5,4 +5,8 @@ class ExperiencesController < ApplicationController
     def load_portfolio_resources
       @portfolio.experiences
     end
+
+    def load_portfolio_resource
+      @portfolio.experiences.find(params[:id])
+    end
 end
