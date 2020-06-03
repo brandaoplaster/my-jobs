fa589ec305ca36a95fbd246410e39b4323ef346b
brandaoplaster <brandaoplaster@gmail.com>
2020-06-02 21:47:56 -0300

    Updating routes including experience on routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources :tags, only: [:create, :destroy], param: :tag_id, controller: :portfolio_tags
     resources :blocks, only: [:index, :create, :destroy]
     resources :profles, only: [:index, :update]
+    resources :experiences, only: [:index, :create, :destroy]
   end
   resources :tags, only: :index
 end
