bbe13c2d08e5c03bbf2c39833cbf2ed0de2f609c
brandaoplaster <brandaoplaster@gmail.com>
2020-06-04 21:29:24 -0300

    Portfolio resource params method created to allow only specific parameters

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -1,2 +1,8 @@
 class EducationsController < ApplicationController
+
+  protected
+
+    def portfolio_resource_params
+      params.require(:education).permit(:title, :institution, :description, :start_date, :end_date)
+    end
 end
