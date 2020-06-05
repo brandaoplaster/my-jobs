dcc8881c04f690c0075814320a894cb33d9cdb97
brandaoplaster <brandaoplaster@gmail.com>
2020-06-04 22:13:52 -0300

    Adding load portfolio resource method

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -2,6 +2,10 @@ class EducationsController < ApplicationController
 
   protected
 
+    def load_portfolio_resource
+      @portfolio.education.find(params[:id])
+    end
+
     def build_portfolio_resource
       @portfolio.education.build
     end
