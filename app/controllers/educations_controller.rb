389b704cc0ad7aed76d4d68bc8b8485474bf28d0
brandaoplaster <brandaoplaster@gmail.com>
2020-06-04 22:12:31 -0300

    Creating build portfolio resource method

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -2,6 +2,10 @@ class EducationsController < ApplicationController
 
   protected
 
+    def build_portfolio_resource
+      @portfolio.education.build
+    end
+
     def portfolio_resource_params
       params.require(:education).permit(:title, :institution, :description, :start_date, :end_date)
     end
