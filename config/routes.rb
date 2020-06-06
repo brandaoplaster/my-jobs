11d3943e1c88a2a3c8a9fdd59408acd78f0bd19a
brandaoplaster <brandaoplaster@gmail.com>
2020-06-05 21:19:55 -0300

    Adding router educations

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resources :blocks, only: [:index, :create, :destroy]
     resources :profles, only: [:index, :update]
     resources :experiences, only: [:index, :create, :destroy]
+    resources :educations, only: [:index, :create, :destroy]
   end
   resources :tags, only: :index
 end
