9c324844cc051ef44ede5930136a934e6c180822
brandaoplaster <brandaoplaster@gmail.com>
2020-06-06 16:33:38 -0300

    Creating build portfolio resource method

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -2,6 +2,10 @@ class SkillsController < ApplicationController
 
   protected
 
+  def build_portfolio_resource
+    @portfolio.skills.build
+  end
+
   def portfolio_resource_params
     params.require(:skill).permit(:title, :percent)
   end
