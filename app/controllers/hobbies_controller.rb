2a2d7da75faa2f7c1df00043d59137dc9f5f46a0
brandaoplaster <brandaoplaster@gmail.com>
2020-06-07 19:29:09 -0300

    Creating build portfolio resource method

--- app/controllers/hobbies_controller.rb
+++ app/controllers/hobbies_controller.rb
@@ -2,6 +2,10 @@ class HobbiesController < ApplicationController
 
   protected
 
+  def build_portfolio_resource
+    @portfolio.hobbies.build
+  end
+
   def portfolio_resource_params
     params.require(:hobby).permit(:title, :percent)
   end
