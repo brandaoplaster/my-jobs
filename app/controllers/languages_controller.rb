b891bfd5ad48e178495c1eb30642829b50c4da3e
brandaoplaster <brandaoplaster@gmail.com>
2020-06-09 22:45:16 -0300

    Creating load portfolio resource method

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -2,6 +2,10 @@ class LanguagesController < ApplicationController
 
   protected
 
+  def load_portfolio_resource
+    @portfolio.languages.find(params[:id])
+  end
+
   def build_portfolio_resource
     @portfolio.languages.build
   end
