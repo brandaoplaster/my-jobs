2739706fef531dc0a2d9859723a27ff39c2c4d21
brandaoplaster <brandaoplaster@gmail.com>
2020-06-09 22:44:09 -0300

    Creating build portfolio resource method

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -2,6 +2,10 @@ class LanguagesController < ApplicationController
 
   protected
 
+  def build_portfolio_resource
+    @portfolio.languages.build
+  end
+
   def portfolio_resource_params
     params.require(:language).permit(:title, :percent)
   end
