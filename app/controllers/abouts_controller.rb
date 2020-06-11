19719578511c6fbb8745afd98f01aba4805485cc
brandaoplaster <brandaoplaster@gmail.com>
2020-06-11 20:56:35 -0300

    Adding build_portfolio_resource method

--- app/controllers/abouts_controller.rb
+++ app/controllers/abouts_controller.rb
@@ -2,6 +2,10 @@ class AboutsController < ApplicationController
 
   protected
 
+  def build_portfolio_resource
+    @portfolio.build_about
+  end
+
   def portfolio_resource_params
     params.require(:about).permit(:description)
   end
