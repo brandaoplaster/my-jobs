a118cd7400249811db8348b3b5574260d1cd2277
brandaoplaster <brandaoplaster@gmail.com>
2020-06-11 21:59:55 -0300

    Adding get and submit methods to the about component

--- app/javascript/packs/components/portfolio_resources/about.vue
+++ app/javascript/packs/components/portfolio_resources/about.vue
@@ -13,3 +13,67 @@
     </div>
   </div>
 </template>
+
+<script>
+import VueCkeditor from "vue-ckeditor2";
+
+export default {
+  components: { VueCkeditor },
+  props: ["portfolioId"],
+
+  data: () => ({
+    about: {},
+    config: {
+      toolbar: [
+        [
+          "Bold",
+          "Italic",
+          "Underline",
+          "Strike",
+          "Subscript",
+          "Superscript",
+          "RemoveFormat",
+          "Cut",
+          "Copy",
+          "Paste",
+          "Undo",
+          "Redo"
+        ]
+      ],
+      height: 150
+    }
+  }),
+
+  created() {
+    this.$resource("/portfolios{/portfolioId}/abouts")
+      .get({ portfolioId: this.portfolioId })
+      .then(
+        response => {
+          this.about = response.body.resource;
+        },
+        response => {
+          M.toast({
+            html:
+              "Ocorreu um erro ao tentar carregar informações do bloco Sobre Mim",
+            classes: "red"
+          });
+        }
+      );
+  },
+
+  methods: {
+    submit() {
+      this.$resource("/portfolios{/portfolioId}/abouts{/id}")
+        .update(
+          { portfolioId: this.portfolioId, id: this.about.id },
+          { about: this.about }
+        )
+        .then({}, response => {
+          response.body.errors.forEach(error => {
+            M.toast({ html: error, classes: "red" });
+          });
+        });
+    }
+  }
+};
+</script>
