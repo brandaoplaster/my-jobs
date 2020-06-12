146b4a8f6cc038db96dede046426cca025d1fbf1
brandaoplaster <brandaoplaster@gmail.com>
2020-06-11 21:40:52 -0300

    Adding component about HTML

---
+++ app/javascript/packs/components/portfolio_resources/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bloco-sobre-mim">
+    <h4 class="block-title grey">Sobre Mim</h4>
+    <div class="row">
+      <div class="input-field col l12 m12 s12">
+        <vue-ckeditor
+          v-model="about.description"
+          :config="config"
+          @blur="submit()"
+          id="about_description"
+        />
+      </div>
+    </div>
+  </div>
+</template>
