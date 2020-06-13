7977017e2f1598a597242aa99dcc4c37cb813fd6
brandaoplaster <brandaoplaster@gmail.com>
2020-06-13 18:03:12 -0300

    Adding router for controller additional information

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :portfolios, except: :new do
+    resources :blocks, only: [:index, :create, :destroy] do
+      resources :additional_informations, only: [:index, :update]
+    end
     resources :tags, only: [:create, :destroy], param: :tag_id, controller: :portfolio_tags
-    resources :blocks, only: [:index, :create, :destroy]
     resources :profles, only: [:index, :update]
     resources :experiences, only: [:index, :create, :destroy]
     resources :educations, only: [:index, :create, :destroy]
