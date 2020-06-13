f939790d06a1e20ea2aa1300e3fc40bd32c91f93
brandaoplaster <brandaoplaster@gmail.com>
2020-06-13 17:22:52 -0300

    Including controller additional information portfolio_resource_params method

--- app/controllers/additional_informations_controller.rb
+++ app/controllers/additional_informations_controller.rb
@@ -1,5 +1,8 @@
 class AdditionalInformationsController < PortfolioResoucesController
 
   protected
-  
+
+  def portfolio_resource_params
+    params.require(:additional_information).permit(:title, :description)
+  end
 end
