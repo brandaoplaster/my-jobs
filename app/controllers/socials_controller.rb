3d378bd82ede469e3ed8151feabb4af010cc20e6
brandaoplaster <brandaoplaster@gmail.com>
2020-06-12 21:55:29 -0300

    Adding build_portfolio_resource method to the social controller

--- app/controllers/socials_controller.rb
+++ app/controllers/socials_controller.rb
@@ -2,6 +2,10 @@ class SocialsController < PortfolioResourcesController
 
   protected
 
+  def build_portfolio_resource
+    @portfolio.socials.buils
+  end
+
   def portfolio_resource_params
     params.require(:social).permit(:kind, :url)
   end
