6915b02ea86fbc544bbeebadb0a0e5e6e1fd30fe
brandaoplaster <brandaoplaster@gmail.com>
2020-06-14 22:19:46 -0300

    Add get and update methods for the additional information component

--- app/javascript/packs/components/portfolio_resources/additional_information.vue
+++ app/javascript/packs/components/portfolio_resources/additional_information.vue
@@ -23,3 +23,72 @@
     </div>
   </div>
 </template>
+
+<script>
+import VueCkeditor2 from "vue-ckeditor2";
+
+export default {
+  components: { VueCkeditor },
+  props: ["portfolioId", "blockId"],
+
+  data: () => ({
+    additional_information: {},
+    config: {
+      toolbar: [
+        [
+          "Bold",
+          "Italic",
+          "Underline",
+          "Strike",
+          "Subscript",
+          "Superscript",
+          "RemoveFormat",
+          "Cut",
+          "Copy",
+          "Paste",
+          "Undo",
+          "Redo"
+        ]
+      ],
+      height: 150
+    }
+  }),
+
+  created() {
+    this.$resource(
+      "/portfolios{/portfolioId}/blocks{/blockId}/additional_informations"
+    )
+      .get({ portfolioId: this.portfolioId, blockId: this.blockId })
+      .then(
+        response => {
+          this.additional_information = response.body.resource;
+        },
+        response => {
+          M.toast({
+            html: "Ocorreu um erro ao tentar carregar Informações Adicionais",
+            classes: "red"
+          });
+        }
+      );
+  },
+
+  methods: {
+    submit() {
+      this.$resource("/portfolios{/portfolioId}/blocks{/blockId}/additional_informations{/id}")
+        .update(
+          {
+            portfolioId: this.portfolioId,
+            blockId: this.blockId,
+            id: this.additional_information.id
+          },
+          { additional_information: this.additional_information }
+        )
+        .then({}, response => {
+          response.body.errors.forEach(error => {
+            M.toast({ html: error, classes: "red" });
+          });
+        });
+    }
+  }
+};
+</script>
