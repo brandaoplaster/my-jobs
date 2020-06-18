8db12d651d6fb27bce1b5a268cc5b64af6ab5889
brandaoplaster <brandaoplaster@gmail.com>
2020-06-17 21:42:13 -0300

    Adding call back load_portfolio and block_params

--- app/controllers/block_positions_controller.rb
+++ app/controllers/block_positions_controller.rb
@@ -1,5 +1,14 @@
 class BlockPositionsController < ApplicationController
+  before_action :load_portfolio
 
   private
   
+  def load_portfolio
+    @portfolio = policy_scope(Portfolio).find(params[:portfolio_id])
+    authorize @portfolio, policy_class: PortfolioResourcePolicy
+  end
+
+  def block_params
+    params.permit(blocks: [:id, :position])
+  end
 end
