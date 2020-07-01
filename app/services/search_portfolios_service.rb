7bf0621c173b768bca88d4ea5f210b2bfe6cd0a3
brandaoplaster <brandaoplaster@gmail.com>
2020-06-30 22:42:46 -0300

    Adding method search_by_place

--- app/services/search_portfolios_service.rb
+++ app/services/search_portfolios_service.rb
@@ -13,5 +13,10 @@ module SearchPortfoliosService
     def search_by_tags
       @basic_search.joins(:tags).where!(tags: { title: @filters[:tag] })
     end
+
+    def search_by_place
+      @basic_search.joins(:contact)
+      @basic_search.where!("contacts.address_country = :place OR contacts.address_city = :place", place: @filters[:place])
+    end
   end
 end
