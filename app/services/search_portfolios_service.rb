2921d2997fc24b59798ff8ebacc5cff50fbba77d
brandaoplaster <brandaoplaster@gmail.com>
2020-06-30 22:40:59 -0300

    Adding method search_by_tags

--- app/services/search_portfolios_service.rb
+++ app/services/search_portfolios_service.rb
@@ -2,10 +2,16 @@ module SearchPortfoliosService
   class << self
     def call(options ={})
       @results = {}
-      @filter = options
+      @filters = options
       perform_basic_search
       perform_featured_search
       @results
+    end
+
+    private
+
+    def search_by_tags
+      @basic_search.joins(:tags).where!(tags: { title: @filters[:tag] })
     end
   end
 end
